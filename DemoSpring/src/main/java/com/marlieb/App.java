a166d49e9da465d2fad9188cbc4e332dba3a0663
JadaMBailey <jbailey0233@gmail.com>
2024-12-28 14:39:09 +0000

    Added steps within the 'Dev' class for a Setter Injection approach using just Spring.

--- DemoSpring/src/main/java/com/marlieb/App.java
+++ DemoSpring/src/main/java/com/marlieb/App.java
@@ -13,9 +13,8 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         Dev obj = (Dev) context.getBean("dev");
-        // Alternative to specifying the bean type required
-//        Dev obj = (Dev) context.getBean("dev");
-//        obj.build();
+
+        System.out.println(obj.getAge());
 
     }
 }
--- DemoSpring/src/main/java/com/marlieb/Dev.java
+++ DemoSpring/src/main/java/com/marlieb/Dev.java
@@ -23,3 +23,9 @@ public class Dev {
         System.out.println("Building something here");
     }
 }
+/*
+SETTER INJECTION : Set values of class variables within Spring.xml
+1 - Create getter and setter methods
+2 - Within the specified instance, add 'property' attribute and specify the name of the var and value wanting to assign.
+3 - OUTPUT VIEW : Specify object and call the getter method to the variable within a System.out.println() function.
+ */
