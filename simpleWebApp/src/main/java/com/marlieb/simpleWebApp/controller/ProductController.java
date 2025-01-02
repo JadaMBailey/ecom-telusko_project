39239b43e601ee362b2466bc0a3882272fba30c3
JadaMBailey <jbailey0233@gmail.com>
2025-01-02 21:41:12 +0000

    ÂI have covered Put / Update method approach using the MVC design pattern, Postman and @ResponseBody annotation.

--- simpleWebApp/src/main/java/com/marlieb/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/src/main/java/com/marlieb/simpleWebApp/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.marlieb.simpleWebApp.controller;
 import com.marlieb.simpleWebApp.model.Product;
 import com.marlieb.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,27 +13,24 @@ public class ProductController {
     @Autowired // used for instance of ProductService
     private ProductService prodService;
 
-    @RequestMapping("/products") // Used for URL path
+    @GetMapping("/products") // Used for URL path
 public List<Product> getProducts(){
     return prodService.getProducts();
 }
 
-@RequestMapping("/products/{prodId}")
+@GetMapping("/products/{prodId}")
 // @PathVariable links up to what is inside here {asb..}
 // , which must be the same as the variable name
 public Product getProductById(@PathVariable int prodId){
         return prodService.getProductById(prodId);
 }
 
+@PostMapping("/products")
+// This annotation is used to match the data being sent from client side to the format set on server side.
+    public void addProduct(@RequestBody Product prod){
+        System.out.println(prod); // Tip to check what is being sent to server viewing on console.
+    // Can additionally check using PostMan and choosing the Get function button to see if product was added
+        prodService.addProduct(prod);
 }
 
-
-
-/*
-1 - URL is typed in 'localhost:8080/products'
-2 - Enters into getProducts method which
-requires entering into ProductService class to retrieve the list of type Product
-3 - Jumps into 'getProducts' method which then jumps to 'products' variable
-4 - that list of data is then returned from ProductService to ProductController
-5 - This is then passed to client to see
- */
+}
--- simpleWebApp/src/main/java/com/marlieb/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/marlieb/simpleWebApp/service/ProductService.java
@@ -3,17 +3,18 @@ package com.marlieb.simpleWebApp.service;
 import com.marlieb.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    // Creating dummy data
-    List<Product> products = Arrays.asList(
+    // Creating dummy data, 'new ArrayList' makes it mutable
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "Iphone 14", 1200),
             new Product(102, "ChromeBook Ultra", 600),
-            new Product(103,"Samsung S24", 1000));
+            new Product(103,"Samsung S24", 1000)));
 
 
     // Want to return a list of products from dummy data
@@ -26,5 +27,9 @@ public class ProductService {
                 // it is fetching the getProductId method which is hidden by Lombok to check against passed prodId
                 .filter(p -> p.getProductId() == prodId)
                 .findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
