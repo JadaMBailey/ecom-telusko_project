3a8473022051ec36972d7f9708d02cd4298ec2b8
JadaMBailey <jbailey0233@gmail.com>
2025-01-09 21:56:20 +0000

    I have covered connecting to a database and setting username, password. I have also replaced the logic in service class to use methods from 'JPARepository' for CRUD operations.

--- simpleWebApp/src/main/java/com/marlieb/simpleWebApp/model/Product.java
+++ simpleWebApp/src/main/java/com/marlieb/simpleWebApp/model/Product.java
@@ -1,12 +1,19 @@
 package com.marlieb.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
 
 @Data // Bundles @Getter, @Setter, @ToString, @EqualsAndHashCode,@RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity // Annotation to use to make an entity
 public class Product {
 
+    @Id // for the variable that will be a primary key
     private int productId;
     private String ProductName;
     private int productPrice;
--- simpleWebApp/src/main/java/com/marlieb/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/marlieb/simpleWebApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.marlieb.simpleWebApp.service;
 
 import com.marlieb.simpleWebApp.model.Product;
+import com.marlieb.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,54 +12,36 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    @Autowired // To get this object, which is spring responsible to create a concrete class that implements 'ProductRepo'
+    ProductRepo repo;
+
     // Creating dummy data, 'new ArrayList' makes it mutable
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101, "Iphone 14", 1200),
-            new Product(102, "ChromeBook Ultra", 600),
-            new Product(103,"Samsung S24", 1000)));
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101, "Iphone 14", 1200),
+//            new Product(102, "ChromeBook Ultra", 600),
+//            new Product(103,"Samsung S24", 1000)));
 
 
     // Want to return a list of products from dummy data
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId) {
-        return products.stream()
-                // it is fetching the getProductId method which is hidden by Lombok to check against passed prodId
-                .filter(p -> p.getProductId() == prodId)
-                .findFirst().get();
+        return repo.findById(prodId).orElse(new Product());
     }
 
     public void addProduct(Product prod) {
-        products.add(prod);
+        repo.save(prod);
     }
 
     public void updateProducts(Product prod) {
-         int index = 0;
-        for (int i = 0; i < products.size(); i++){
-             if(products.get(i).getProductId() == prod.getProductId())
-                 index = i;
-            products.set(index,prod);
-            /*
-            1 - for loop to run through each product
-            2 - conditional statement to check product id is equal to product being updated.
-            3 - If yes the position of the product in the list is now equal to variable 'index'
-            4 - Using the set method, can associate the index with the local variable being passed
-             */
+         repo.save(prod); // if data is there it will update, if no it will create (addProduct)
          }
 
-
-    }
-
     public void deleteProduct(int productId) {
-        int index = 0;
-
-        for (int i = 0; i < products.size(); i++)
-            if(products.get(i).getProductId() == productId)
-                index = i;
-
-        products.remove(index);
-
+        repo.deleteById(productId);
     }
-}
+    }
+
+
