841c68c79cc7d125f2cb8bf6dd204e91560ca5c7
JadaMBailey <jbailey0233@gmail.com>
2025-01-17 18:52:34 +0000

    Created the columns for the database table in model directory stored in 'Product' class with the use of the @Entity annotation.

---
+++ e-com_project/src/main/java/com/marlieb/e_com_project/model/Product.java
@@ -0,0 +1,28 @@
+package com.marlieb.e_com_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price; // 'BigDecimal' as the price can be any big amount
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+}
